2478d7dffc69fc849d57c781b2e8bfb63f1d9cce
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2015-11-01 05:43:30 -0500

    chrome extension added

---
+++ appnew/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  if (localStorage.getItem("moneySpent") == undefined){
+    localStorage.setItem("moneySpent", 0);
+  }
+   var moneySpent = localStorage["moneySpent"];
+
+   if (moneySpent < 3) {
+     document.getElementById('title').innerHTML = "You don't waste too much time, keep it up.";
+   } else {
+     document.getElementById('title').innerHTML = "Goddamn, go faceYOURbook";
+   }
+
+   document.getElementById('money').innerHTML = moneySpent;
+
+ });
