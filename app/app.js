d7de62cb0a29b2234ffc319035c02b21bb668374
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2015-10-31 22:05:44 -0400

    fixed bug, new page works

--- app/app.js
+++ app/app.js
@@ -1,23 +1,16 @@
-// save amount of money spent to local storage
-// is this the initializer??
-//
-function loadApp() {
-   if (localStorage.getItem("moneySpent") == undefined){
-     localStorage.setItem("moneySpent") = 0;
+document.addEventListener('DOMContentLoaded', function(){
+
+  if (localStorage.getItem("moneySpent") == undefined){
+    localStorage.setItem("moneySpent", 0);
+  }
+   var moneySpent = localStorage["moneySpent"];
+
+   if (moneySpent < 3) {
+     document.getElementById('title').innerHTML = "You don't waste too much time, keep it up.";
+   } else {
+     document.getElementById('title').innerHTML = "Goddamn, go faceYOURbook";
    }
-    var moneySpent = localStorage["moneySpent"];
 
-    if (moneySpent < 3) {
-      document.getElementById('titel').innerHTML = "You don't waste too much time, keep it up.";
-    } else {
-      document.getElementById('title').innerHTML = "Goddamn, go faceYOURbook";
-    }
+   document.getElementById('money').innerHTML = moneySpent;
 
-    document.getElementById('money').innerHTML = moneySpent;
-}
-
-
-// function addDolar() {
-//   var dollarSpent = localStorage.moneySpent;
-//   dollarSpent++;
-// }
+ });
